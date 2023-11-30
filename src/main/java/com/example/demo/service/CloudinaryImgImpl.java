4a28def7a6854fb57f72a70dee525b2b0912f455
yasuoOwO <heinmina05@gmail.com>
2023-11-30 23:16:07 +0630

    payment process

--- src/main/java/com/example/demo/service/CloudinaryImgImpl.java
+++ src/main/java/com/example/demo/service/CloudinaryImgImpl.java
@@ -9,37 +9,37 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.UUID;
 
 import com.cloudinary.Cloudinary;
+
 @Service
 public class CloudinaryImgImpl implements CloudinaryImageService {
 	@Autowired
 	Cloudinary cloudinary;
 
 	@Override
-    public String uploadFile(MultipartFile multipartFile) throws IOException {
-        return cloudinary.uploader()
-                .upload(multipartFile.getBytes(),
-                        Map.of("public_id", UUID.randomUUID().toString()))
-                .get("url")
-                .toString();
-    }
-	
+	public String uploadFile(MultipartFile multipartFile) throws IOException {
+		return cloudinary.uploader().upload(multipartFile.getBytes(), Map.of("public_id", UUID.randomUUID().toString()))
+				.get("url").toString();
+	}
+
 	@Override
 	public Boolean deleteFile(String imgUrl) {
 		try {
-	        String publicId = extractPublicId(imgUrl);
+			String publicId = extractPublicId(imgUrl);
 
-	        cloudinary.uploader().destroy(publicId, null);
+			cloudinary.uploader().destroy(publicId, null);
 
-	        return true; // Deletion successful
-	    } catch (Exception e) {
-	        e.printStackTrace(); // You might want to log the exception or handle it appropriately
-	        return false; // Deletion failed
-	    }
+			return true; // Deletion successful
+		} catch (Exception e) {
+			e.printStackTrace(); // You might want to log the exception or handle it appropriately
+			return false; // Deletion failed
+		}
 	}
-	
+
 	private String extractPublicId(String imgUrl) {
-	    int startIndex = imgUrl.lastIndexOf("/") + 1;
-	    int endIndex = imgUrl.lastIndexOf(".");
-	    return imgUrl.substring(startIndex, endIndex);
+		int startIndex = imgUrl.lastIndexOf("/") + 1;
+		int endIndex = imgUrl.lastIndexOf(".");
+		return imgUrl.substring(startIndex, endIndex);
 	}
+
+
 }
