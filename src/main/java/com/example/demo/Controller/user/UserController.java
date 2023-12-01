6d2a27ef56b064e89acffa671689b253568a594c
ZarZarHlaing123 <zarzarhlaing182@gmail.com>
2023-12-01 21:26:13 +0630

    userHistory

--- src/main/java/com/example/demo/Controller/user/UserController.java
+++ src/main/java/com/example/demo/Controller/user/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.Controller.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -7,10 +9,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.daos.OrderProductRepository;
+import com.example.demo.daos.OrderRepository;
 import com.example.demo.daos.UserRepository;
+import com.example.demo.model.Order;
+import com.example.demo.model.OrderProducts;
 import com.example.demo.model.User;
 import com.example.demo.service.UserOwnDetail;
 
@@ -20,6 +27,12 @@ import jakarta.servlet.http.HttpSession;
 public class UserController {
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	OrderRepository orderRepository;
+	
+	@Autowired
+	OrderProductRepository orderProductRepository;
 	
 	@GetMapping("/home")
 	public String home(Model model,HttpSession session) {
@@ -112,5 +125,26 @@ public class UserController {
 
 	    return "user/account/changePassword";
 	}
+	
+	@GetMapping("/user/orderhistory")
+	public String ShowOrder(String query,Model model) {
+		List<Order> orders;
+
+		if (query != null && !query.isEmpty()) {
+			orders = orderRepository.findByOrderNumberContainingIgnoreCase(query.trim());
+		} else {
+			orders = orderRepository.findAll();
+		}
+		model.addAttribute("orders", orders);
+		return "user/account/orderHistory";
+
+	}
+	@GetMapping("/orderhistory/detail/{id}")
+	public String viewDetail(@PathVariable("id") Integer id, Model model) {
+		List<OrderProducts> orderProducts = orderProductRepository.findByOrderId(id);
+		model.addAttribute("orderProducts", orderProducts);
+		return "user/account/orderDetail";
+	}
+	
 	
 }
