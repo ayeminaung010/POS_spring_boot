6476a9c2c96064c327655fe46d4d18530dc85fb1
yasuoOwO <heinmina05@gmail.com>
2023-12-01 11:00:08 +0630

    product fix

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -53,7 +52,7 @@ public class Product {
 	private double discount;
 
 	//join table
-	@ManyToOne(cascade = CascadeType.PERSIST)
+	@ManyToOne
 	@JoinColumn(name = "brandId", referencedColumnName = "brandId")
 	private Brand brand;
 
