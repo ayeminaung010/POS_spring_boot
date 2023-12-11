8df2c3e4027f562f09f218a96b96495eededc583
ZarZarHlaing123 <zarzarhlaing182@gmail.com>
2023-12-11 21:51:25 +0630

    outofstock

--- src/main/java/com/example/demo/daos/ProductRepository.java
+++ src/main/java/com/example/demo/daos/ProductRepository.java
@@ -4,6 +4,7 @@ package com.example.demo.daos;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -32,4 +33,15 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 		       "OR LOWER(CAST(p.discount AS string)) LIKE LOWER(CONCAT('%', :query, '%'))) " +
 		       "AND p.discount > 0")
 		Page<Product> findByDiscountContainingIgnoreCase(@Param("query") String query, Pageable pageable);
+
+	Page<Product> findByStock(Integer stock, PageRequest pageRequest);
+	
+	@Query("SELECT p FROM Product p " +
+		       "WHERE (LOWER(p.name) LIKE LOWER(CONCAT('%', :query, '%')) " +
+		       "OR LOWER(p.description) LIKE LOWER(CONCAT('%', :query, '%')) " +
+		       "OR LOWER(CAST(p.discount AS string)) LIKE LOWER(CONCAT('%', :query, '%'))) " +
+		       "AND p.stock <= 0")
+		Page<Product> findByOutofstockContainingIgnoreCase(@Param("query") String query, Pageable pageable);
+
+	
 }
